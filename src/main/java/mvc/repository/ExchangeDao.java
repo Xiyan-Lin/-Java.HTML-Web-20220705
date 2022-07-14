f60f85770c2ea1be1bbb718e7c050ac30cfa9ec5
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-14 20:07:20 +0800

    s

--- src/main/java/mvc/repository/ExchangeDao.java
+++ src/main/java/mvc/repository/ExchangeDao.java
@@ -1,5 +1,20 @@
 package mvc.repository;
 
-public class ExchangeDao {
+import java.io.IOException;
 
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+
+public class ExchangeDao {
+	public Stock get(String symbol) throws IOException {
+		Stock stock = YahooFinance.get(symbol);
+		return stock;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		//String symbol = "USDTWD=x";  // x -> Exchange
+		String symbol = "2356.TW";
+		ExchangeDao dao = new ExchangeDao();
+		System.out.println(dao.get(symbol));
+	}
 }

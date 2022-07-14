27a0e0701c28032dfae83257b2c4c16a606f0e41
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-14 20:32:09 +0800

    s

--- src/main/java/mvc/repository/ExchangeDao.java
+++ src/main/java/mvc/repository/ExchangeDao.java
@@ -11,10 +11,4 @@ public class ExchangeDao {
 		return stock;
 	}
 	
-	public static void main(String[] args) throws IOException {
-		//String symbol = "USDTWD=x";  // x -> Exchange
-		String symbol = "2356.TW";
-		ExchangeDao dao = new ExchangeDao();
-		System.out.println(dao.get(symbol));
-	}
 }

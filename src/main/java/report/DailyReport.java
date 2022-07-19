c8389e32f71acbc0e9acd7bf03dfbb5fd57aac12
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-19 20:01:47 +0800

    a

---
+++ src/main/java/report/DailyReport.java
@@ -0,0 +1,30 @@
+package report;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/report/daily")
+public class DailyReport extends HttpServlet {
+	
+	private void doHandle(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.print("<h1>日報表</h1>登入成功之後才可以看~");
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doHandle(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doHandle(req, resp);
+	}
+	
+}

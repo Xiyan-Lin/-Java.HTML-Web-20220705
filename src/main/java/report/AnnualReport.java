98fa6644cea0778c7751959356506400c18b3879
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-19 21:19:39 +0800

    s

---
+++ src/main/java/report/AnnualReport.java
@@ -0,0 +1,30 @@
+package report;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/report/annul")
+public class AnnualReport extends HttpServlet {
+	
+	private void doHandle(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.print("<h1>年報表</h1>登入成功之後才可以看~");
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doHandle(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doHandle(req, resp);
+	}
+	
+}

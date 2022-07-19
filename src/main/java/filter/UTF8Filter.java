6baf1c77e89d1d84cd067a67f6c6381b41247141
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-19 20:12:32 +0800

    s

---
+++ src/main/java/filter/UTF8Filter.java
@@ -0,0 +1,29 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+// 定義要過濾的路徑
+//@WebFilter(urlPatterns = {"/report/daily", "/report/monthly"})
+@WebFilter(urlPatterns = {"/report/*"})
+public class UTF8Filter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// UTF-8 配置
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=utf-8");
+		// 往下傳送
+		chain.doFilter(request, response);
+		
+	}
+	
+}

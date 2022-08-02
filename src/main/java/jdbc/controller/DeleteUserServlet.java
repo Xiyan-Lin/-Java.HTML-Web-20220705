3edb615f540558b3ee17009fb4cec6f1d1861ed4
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-02 21:41:02 +0800

    s

---
+++ src/main/java/jdbc/controller/DeleteUserServlet.java
@@ -0,0 +1,30 @@
+package jdbc.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jdbc.service.UserService;
+
+@WebServlet("/user/delete")
+public class DeleteUserServlet extends HttpServlet {
+	
+	private UserService userService = new UserService();
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Integer id = Integer.valueOf(req.getParameter("id"));
+		int rowcount = userService.delete(id);
+		
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/user/success");
+		req.setAttribute("action", "delete");
+		req.setAttribute("rowcount", rowcount);
+		rd.forward(req, resp);
+	}
+	
+}
---
+++ src/main/java/jdbc/controller/GetUserServlet.java
@@ -0,0 +1,30 @@
+package jdbc.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jdbc.entity.User;
+import jdbc.service.UserService;
+
+@WebServlet("/user/get")
+public class GetUserServlet extends HttpServlet {
+	
+	private UserService userService = new UserService();
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Integer id = Integer.valueOf(req.getParameter("id"));
+		User user = userService.getUser(id);
+		
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/user/update");
+		req.setAttribute("user", user);
+		rd.forward(req, resp);
+	}
+	
+}

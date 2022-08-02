b3b213f16b957eab1a45ff55c44540aca7996829
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-02 20:08:18 +0800

    s

--- src/main/java/jdbc/repository/UserDao.java
+++ src/main/java/jdbc/repository/UserDao.java
@@ -2,7 +2,12 @@ package jdbc.repository;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.PseudoColumnUsage;
 import java.sql.Statement;
+import java.util.Base64;
+
+import jdbc.entity.User;
 
 public class UserDao {
 	// 1. 資料庫放置位置
@@ -11,7 +16,14 @@ public class UserDao {
 	private String db_name = "webdb.db";
 	// 3. 建立資料庫連線參數
 	private String db_url = "jdbc:sqlite:" + db_path + db_name;
-			
+	
+	// 取得 Connection 物件
+	private Connection getConnection() throws Exception {
+		Class.forName("org.sqlite.JDBC");
+		Connection conn = DriverManager.getConnection(db_url);
+		return conn;
+	}
+		
 	// 建立 DB
 	public void createDB() throws Exception {
 		// 4. 建立資料庫 Driver
@@ -22,12 +34,6 @@ public class UserDao {
 			System.out.println("資料庫建立/連線成功");
 		}
 		conn.close();
-	}
-	
-	private Connection getConnection() throws Exception {
-		Class.forName("org.sqlite.JDBC");
-		Connection conn = DriverManager.getConnection(db_url);
-		return conn;
 	}
 	
 	// 建立資料表 Table
@@ -44,8 +50,29 @@ public class UserDao {
 		System.out.println("user 資料表建立完成 !");
 	}
 	
+	// 新增 user 資料列
+	public int add(User user) {
+		String sql = "insert into user(username, password) values(?, ?)";
+		int rowcount = 0;
+		try(PreparedStatement pstmt = getConnection().prepareStatement(sql)) {
+			pstmt.setString(1, user.getUsername());
+			pstmt.setString(2, user.getPassword());
+			rowcount = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return rowcount;
+	}
+	
+	
 	public static void main(String[] args) throws Exception {
-		new UserDao().createTable();
+		//new UserDao().createDB();
+		//new UserDao().createTable();
+		String password = "1234";
+		new UserDao().add(new User("john", Base64.getEncoder().encodeToString(password.getBytes())));
+		password = "5678";
+		new UserDao().add(new User("mary", Base64.getEncoder().encodeToString(password.getBytes())));
+		
 	}
 	
 }

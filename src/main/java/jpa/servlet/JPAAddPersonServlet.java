a59e3bbc63c0f657be60bdafed91b4920bdb8d29
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-09 20:11:14 +0800

    s

---
+++ src/main/java/jpa/servlet/JPAAddPersonServlet.java
@@ -0,0 +1,47 @@
+package jpa.servlet;
+
+import java.io.IOException;
+import java.util.Random;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.github.javafaker.Faker;
+
+import jpa.entity.Person;
+import jpa.service.JPAService;
+
+@WebServlet("/jpa/person/add")
+public class JPAAddPersonServlet extends HttpServlet {
+	
+	private JPAService jpaService;
+
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		jpaService = new JPAService();
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String name = new Faker().name().lastName();
+		Integer age = new Random().nextInt(20) + 20;
+		
+		Person person = new Person();
+		person.setName(name);
+		person.setAge(age);
+		
+		jpaService.addPerson(person);
+		
+		resp.getWriter().print("Add ok: " + person);
+		
+	}
+	
+	
+	
+	
+}

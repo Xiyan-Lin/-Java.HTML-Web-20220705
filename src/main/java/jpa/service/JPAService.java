7026fbc65b821b21b70bc76168f49a5023771820
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-09 20:06:42 +0800

    s

---
+++ src/main/java/jpa/service/JPAService.java
@@ -0,0 +1,27 @@
+package jpa.service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import jpa.entity.Person;
+
+public class JPAService {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	
+	public JPAService() {
+		if(emf == null) {
+			emf = Persistence.createEntityManagerFactory("demo");
+		}
+		em = emf.createEntityManager();
+	}
+	
+	public synchronized void addPerson(Person person) {
+		EntityTransaction etx = em.getTransaction(); // 取得交易物件(交易:新增,修改,刪除)
+		etx.begin(); // 開始
+		em.persist(person);  // 存入 person
+		etx.commit(); // 提交
+	}
+}

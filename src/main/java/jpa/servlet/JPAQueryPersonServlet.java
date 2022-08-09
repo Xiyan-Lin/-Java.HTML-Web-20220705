99e54b37544416189afff9a0fd8dc6b6dce83b1d
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-09 21:41:06 +0800

    s

--- src/main/java/jpa/servlet/JPAQueryPersonServlet.java
+++ src/main/java/jpa/servlet/JPAQueryPersonServlet.java
@@ -15,9 +15,10 @@ public class JPAQueryPersonServlet extends JPABaseServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter out = resp.getWriter();
 		//out.println(getJPAService().getPerson(1));
-		out.println(getJPAService().queryAllPerson());
-		out.println(getJPAService().queryPersonByAge(30));
-		
+		//out.println(getJPAService().queryAllPerson());
+		//out.println(getJPAService().queryPersonByAge(30));
+		out.println(getJPAService().findByName("%ch%"));
+		out.println(getJPAService().findByAgeBetween(28, 32));
 	}
 	
 	

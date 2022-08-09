1ad8496029a59a02f36a5b6f03bcbd1c7d058017
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-09 19:40:20 +0800

    s

---
+++ src/main/java/jpa/entity/Person.java
@@ -0,0 +1,57 @@
+package jpa.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "person")
+public class Person implements Serializable {
+	
+	@Id // 主鍵設定
+	@GeneratedValue(strategy = GenerationType.AUTO) // 主鍵內容新增策略
+	private Integer id;
+	
+	@Column(name = "name", length = 50, nullable = false, unique = true) // 資料欄位設定
+	private String name; 
+	
+	@Column
+	private Integer age;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
+	
+	
+	
+}

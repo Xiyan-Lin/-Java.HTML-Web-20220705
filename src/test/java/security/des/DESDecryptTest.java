b940ba74c6e7cada7aae66378b89bf28e31c6d42
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-23 21:02:37 +0800

    web

--- src/test/java/security/des/DESDecryptTest.java
+++ src/test/java/security/des/DESDecryptTest.java
@@ -7,12 +7,13 @@ public class DESDecryptTest {
 	
 	public static void main(String[] args) throws Exception {
 		// 設定密鑰的路徑與名稱
-		String key_path = "C:/Users/MB-207/eclipse-workspace/JavaWeb_20220705/key/user.key";
+		String key_path = "C:/Users/MB-207/eclipse-workspace/JavaWeb_20220705/key/user2.key";
 		// 建立 DESEncryptService
 		DESEncryptService des = new DESEncryptService(key_path);
 		
 		// 待解密的 hex
-		String hex = "560df430de1dd0451f55021b79011154ab276a8230fd78c71d0f093c6cc49da9ba07e06928761eefece3ec5a106fb3a9";
+		//String hex = "560df430de1dd0451f55021b79011154ab276a8230fd78c71d0f093c6cc49da9ba07e06928761eefece3ec5a106fb3a9";
+		String hex = "f83bf80e0af28fb968ddcd3e8e22e59d6a222edacf3a9b7f";
 		System.out.println("待解密的 hex: " + hex);
 		
 		// 將 hex 轉 byte[]
--- src/test/java/security/des/DESEncryptTest.java
+++ src/test/java/security/des/DESEncryptTest.java
@@ -7,12 +7,13 @@ public class DESEncryptTest {
 	
 	public static void main(String[] args) throws Exception {
 		// 設定密鑰的路徑與名稱
-		String key_path = "C:/Users/MB-207/eclipse-workspace/JavaWeb_20220705/key/user.key";
+		String key_path = "C:/Users/MB-207/eclipse-workspace/JavaWeb_20220705/key/user2.key";
 		// 建立 DESEncryptService
 		DESEncryptService des = new DESEncryptService(key_path);
 		
 		// 明文
-		String msg = "跟家人中秋賞月吃月餅吃燒烤...";
+		//String msg = "跟家人中秋賞月吃月餅吃燒烤...";
+		String msg = "聖誕節快到了...";
 		System.out.println("明文: " + msg);
 		
 		// 加密
